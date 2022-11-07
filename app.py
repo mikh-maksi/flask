2a03cbd55b988520643fd8535af53fc74c5b24e8
Mikhail <newlevelcenter@gmail.com>
2022-11-07 09:31:15 +0200

    start

--- app.py
+++ app.py
@@ -1,12 +1,79 @@
 from flask import Flask, render_template
 import json
+from flask_marshmallow import Marshmallow
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import *
 
+db = create_engine('postgresql+psycopg2://jjjvxftvljouqa:a41a7e3069600c2daa7ed7e917e26216fbd28f517719f517c4529b994bb8e430@ec2-34-248-169-69.eu-west-1.compute.amazonaws.com:5432/dfmbp1l6kre1rn')
+# create the app
 app = Flask(__name__)
+
+CORS(app, supports_credentials=True, allow_headers=True)
+app.config['CELERY_BROKER_URL'] = 'redis://localhost:6379/0'
+app.config['CELERY_RESULT_BACKEND'] = 'redis://localhost:6379/0'
+
+celery = Celery(app.name, broker=app.config['CELERY_BROKER_URL'])
+celery.conf.update(app.config)
+
+base = declarative_base()
+
+Session = sessionmaker(db)
+session = Session()
+
+@app.cli.command('db_create')
+def db_create():
+    base.metadata.create_all(db)
+    print('Database created')
+
+ma = Marshmallow(app)
+
+
+class Answers(base):
+    __tablename__ = 'answers'
+    id = Column(Integer, primary_key=True)
+    title = Column(String(100), nullable=False)
+    a1 = Column(String(50), nullable=False)
+    a2 = Column(String(50), nullable=False)
+    a3 = Column(String(50), nullable=False)
+    a4 = Column(String(50), nullable=False)
+    answer_name = Column(String(50), nullable=False)
+    answer_n = Column(Integer, nullable=False)
+    date_time = Column(DateTime, nullable=False)
+
+class AnswersSchema(ma.Schema):
+    class Meta:
+        fields = ('id', 'title', 'a1', 'a2', 'a3', 'a4', 'answer_name', 'answer_n', 'date_time')
+
+answers_schema = AnswersSchema()
 
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
 
+
+
+
+@app.route("/get_answer")
+def get_answer():
+    title = "Title"
+    a1 = 'a1'
+    a2 = 'a2'
+    a3 = 'a3'
+    a4 = 'a4'
+    answer_name = 'answer_name'
+    answer_n = 1
+    date_time = '2022-11-07'
+    answer = Answers( title=title , a1=a1,
+                a2=a2, a3=a3, a4=a4,answer_name=answer_name, answer_n=answer_n, date_time=date_time)
+    session.add(answer)
+    session.commit()
+    test = session.query(Answers).filter_by(id=1).first()
+    data = AnswersSchema.dump(test)
+    print(data)
+
+    return jsonify(data=data, message=f'Report {answer.id} successfully registered'), 202
 
 @app.route("/save")
 def save():
